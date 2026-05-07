d130da00bc17a9fbcc7f8ccd54384e00b490dee7
Harry Marshall <harryyymarshall@gmail.com>
2026-05-07 12:20:57 +1000

    chore: project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-ci-off-white flex items-center justify-center">
+      <h1 className="text-2xl font-semibold text-slate-700">
+        Niagara Building Management System
+      </h1>
+    </div>
+  );
+}
